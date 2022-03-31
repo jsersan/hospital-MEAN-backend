34a2e52e449d5d2c535a67b63997cfb98afc6e4a
jsersan <jsersan@gmail.com>
2022-03-31 20:18:44 +0200

    SPA Control

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.use( express.json() );
 app.use(express.static('public'))
 
 
+// path
+
+const path = require('path');
+
 // Rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
 app.use('/api/hospitales',require('./routes/hospitales'));
@@ -36,6 +40,12 @@ app.use('/api/medicos',require('./routes/medicos'));
 app.use('/api/todo',require('./routes/busquedas'));
 app.use('/api/login',require('./routes/auth'));
 app.use('/api/upload',require('./routes/uploads'));
+
+// Lo último
+
+app.get('*', (req,res) =>{
+    res.sendFile( path.resolve(__dirname, 'public/index.html'));
+});
 
 
 // Para levantar el servidor para que escuche por el puerto :
